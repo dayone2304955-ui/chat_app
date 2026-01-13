ad379cdf57ed022739891525e7c11b380f6c0d24
Phongsakhon Tongcham <dayone2304955@gmail.com>
2026-01-13 12:14:49 +0700

    Change ThemePlate to Cool! Ver2

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "bad6bb144be46c0aad75d8b5a77ba9f7",
+"flutter_bootstrap.js": "2da7e06f99e37d1ba3fe4cbd2353aaa7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "fb54d11a54a7e7c93d3808b9be6c98cc",
 "/": "fb54d11a54a7e7c93d3808b9be6c98cc",
-"main.dart.js": "86ce8a32ae00d642e913fadd0542f030",
+"main.dart.js": "be35f53b0ab0adf286e4cd96103eee88",
 "manifest.json": "f55bb5697e7d4178d10bc43722f4d748",
 "version.json": "3db4d3558c0a11dc4942934a8f4524b7"};
 // The application shell files that are downloaded before a service worker can
