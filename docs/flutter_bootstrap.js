7ebe6c0ab55a9068108d7cca5b0745253c5380cf
Phongsakhon Tongcham <dayone2304955@gmail.com>
2026-01-13 11:59:35 +0700

    Change ThemePlate to Cool!

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "997782872"
+    serviceWorkerVersion: "858065375"
   }
 });
